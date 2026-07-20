279fc5e95c225b2c115b5bce4238584389e9276f
Dmytro Kovbasiuk <159040488+DmytroK-goit@users.noreply.github.com>
2026-07-20 13:03:48 +0300

    first commit

---
+++ src/component/footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <section className="bg-amber-900">
+      <p className="text-amber-50">Footer</p>
+    </section>
+  );
+}
---
+++ src/component/header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <section className="bg-amber-500">
+      <p className="text-amber-50">Header</p>
+    </section>
+  );
+}
